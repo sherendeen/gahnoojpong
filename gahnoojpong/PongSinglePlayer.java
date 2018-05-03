a3744136ded3faaba382acb5bc2477b36c550d8e
sherendeen <inivican@hotmail.com>
2018-05-02 23:41:07 -0400

    Cleaned up the code a little bit

--- gahnoojpong/PongSinglePlayer.java
+++ gahnoojpong/PongSinglePlayer.java
@@ -13,18 +13,33 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+/*
+    Javafx look-alike implementation of a classic arcade game that rhymes with "wrong"
+    Copyright (C) 2018 Seth G. R. Herendeen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 /**
  * 
  * @author Seth G. R. Herendeen
- *	Java struct to hold most relevant variables in the class.
- *	
  */
 class GameStruct {
 	public Line line = this.createCenterLine();
 	public Rectangle playerOne = this.createPlayerOne();
 	public Rectangle playerTwo = this.createPlayerTwo();
 	public Circle ball = this.createBall();
-	
+
 	public int speedX = 3;
 	public int speedY = 3;
 	public int directionVelocity = this.speedX;
@@ -66,9 +81,10 @@ class GameStruct {
 /**
  * 
  * @author <b>Seth G. R. Herendeen</b> <br />
- * 		References/inspirations:
+ *         References/inspirations:
  *         <li>https://youtu.be/n5Kz5CcquXs</li>
  *         <li>http://www.dummies.com/programming/java/javafx-binding-properties/</li>
+ *         <li>A pong game that I made in ~2016 in C#</li>
  *
  */
 public class PongSinglePlayer extends Application {
@@ -82,22 +98,22 @@ public class PongSinglePlayer extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		primaryStage.setTitle("JPong: Single-player mode");
 		primaryStage.setScene(new Scene(createContent()));
 		primaryStage.show();
 		primaryStage.getScene().setOnKeyPressed(e -> {
 			handleKeyPresses(e);
 		});
-			
+
 	}
 
 	private void handleKeyPresses(KeyEvent e) {
-		if(e.getCode() == KeyCode.UP)
-			this.struct.playerTwo.setLayoutY(this.struct.playerTwo.getLayoutY()-30);
-		if(e.getCode() == KeyCode.DOWN)
-			this.struct.playerTwo.setLayoutY(this.struct.playerTwo.getLayoutY()+30);
-		
+		if (e.getCode() == KeyCode.UP)
+			this.struct.playerTwo.setLayoutY(this.struct.playerTwo.getLayoutY() - 30);
+		if (e.getCode() == KeyCode.DOWN)
+			this.struct.playerTwo.setLayoutY(this.struct.playerTwo.getLayoutY() + 30);
+
 	}
 
 	private Parent createContent() {
@@ -105,12 +121,6 @@ public class PongSinglePlayer extends Application {
 		pane.setPrefSize(this.struct.WIDTH, this.struct.HEIGHT);
 		pane.setStyle("-fx-background: black");
 
-		// this.struct.line = createCenterLine();
-		// this.struct.playerOne = createPlayerOne();
-		// this.struct.playerTwo = createPlayerTwo();
-		// this.struct.ball = createBall();
-
-		
 		pane.getChildren().addAll(this.struct.line, this.struct.playerOne, this.struct.playerTwo, this.struct.ball);
 		this.timer = new AnimationTimer() {
 
@@ -120,55 +130,10 @@ public class PongSinglePlayer extends Application {
 
 			}
 		};
-		this.timer.start();
-		
+		this.timer.start();// this line was imperative
+
 		return pane;
 	}
-
-	// public Rectangle createPlayerOne() {
-	// Rectangle playerOne = new Rectangle(10, 80, Color.BLACK);
-	// playerOne.setStroke(Color.WHITE);
-	// playerOne.setLayoutX(0);
-	// playerOne.setLayoutY(this.struct.HEIGHT/2-40);
-	// return playerOne;
-	// }
-	//
-	// public Rectangle createPlayerTwo() {
-	// Rectangle playerTwo = new Rectangle(10,80,Color.WHITE);
-	// playerTwo.setLayoutX(this.struct.WIDTH - 10);
-	// playerTwo.setLayoutY(this.struct.HEIGHT/2 - 40);
-	// return playerTwo;
-	// }
-	//
-	// public Circle createBall() {
-	// Circle ball = new Circle(10);
-	// ball.setFill(Color.GREEN);
-	// ball.setLayoutX(this.struct.WIDTH/2);
-	// ball.setLayoutY(this.struct.HEIGHT/2);
-	// return ball;
-	// }
-	//
-	// public Line createCenterLine() {
-	// Line centerLine = new
-	// Line(this.struct.WIDTH/2,0,this.struct.WIDTH/2,this.struct.HEIGHT);
-	// centerLine.setStroke(Color.WHITE);
-	// return centerLine;
-	// }
-
-	/**
-	 * Sets up the timer
-	 */
-//	private void setUpTimer() {
-//		this.timer = new AnimationTimer() {
-//
-//			@Override
-//			public void handle(long now) {
-//				updateAndRedraw();
-//
-//			}
-//		};
-//
-//	}
 
 	private void updateAndRedraw() {
 		double x = this.struct.ball.getLayoutX();
